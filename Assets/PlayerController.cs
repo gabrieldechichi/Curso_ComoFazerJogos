66fe03424aeb73ae65ec6fc4ceb525ba6d885bd8
gabriel_dechichi <gabrieldechichi@gmail.com>
2020-02-02 19:31:10 -0700

    Video 1, 2 e 3

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(0, 200, 2000);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
