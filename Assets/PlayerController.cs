a945d852211f7e2fe6991ba55f57cceced4a36f3
gabriel_dechichi <gabrieldechichi@gmail.com>
2020-02-04 21:38:08 -0700

    Videos 4 e 5

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
+    public int forcaEmX;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb.AddForce(0, 200, 2000);
+        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetKey("a") == true)
+        {     
+            rb.AddForce(-forcaEmX* Time.fixedDeltaTime, 0, 0);
+            Debug.Log("O A esta apertado");
+        }
+
+        if (Input.GetKey("d") == true)
+        {
+            rb.AddForce(forcaEmX*Time.fixedDeltaTime, 0, 0);
+        }
     }
 }
