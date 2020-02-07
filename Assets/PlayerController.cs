115bcca5b823d7ac5d9ed5a4fe53a77a3222162e
gabriel_dechichi <gabrieldechichi@gmail.com>
2020-02-06 19:31:54 -0700

    Videos 6 e 7

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
     public int forcaEmX;
+    public int forcaEmZ = 50;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        rb.AddForce(0, 0, forcaEmZ*Time.fixedDeltaTime);
         if (Input.GetKey("a") == true)
         {     
             rb.AddForce(-forcaEmX* Time.fixedDeltaTime, 0, 0);
