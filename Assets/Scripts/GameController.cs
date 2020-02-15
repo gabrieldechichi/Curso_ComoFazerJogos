9a9d3314813c8c44e9f2ffea586ec66fd84ca4eb
gabriel_dechichi <gabrieldechichi@gmail.com>
2020-02-15 14:40:49 -0700

    Videos 12 e 13

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,22 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    public Text textoPontuacao;
+    public PlayerController player;
+    Vector3 posicaoInicial;
+    public float divisorDaPontuacao;
+    public GameObject painelGameOver;
     // Start is called before the first frame update
     void Start()
     {
-        
+        posicaoInicial = player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 distanciaPercorrida = player.transform.position - posicaoInicial;
+        float pontuacao = distanciaPercorrida.z / divisorDaPontuacao;
+        textoPontuacao.text = pontuacao.ToString("0");
     }
 
     public void GameOver()
+    {
+        painelGameOver.SetActive(true);
+        Invoke("RecarregarLevel", 2);
+    }
+
+    public void RecarregarLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
